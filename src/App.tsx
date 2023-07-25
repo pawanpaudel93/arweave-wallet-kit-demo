4029fdaf95e1718c90ac454eb7210588495acbab
Pawan Paudel <pawanpaudel93@gmail.com>
2023-07-26 00:46:23 +0545

    feat: Add react dropzone and upload the selected images

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,158 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { ConnectButton, useApi, useConnection } from "arweave-wallet-kit";
-import { useEffect, useState } from "react";
-import "./App.css";
+import {
+  Container,
+  Flex,
+  Box,
+  Button,
+  VStack,
+  useToast,
+} from "@chakra-ui/react";
+import { useDropzone } from "react-dropzone";
+import { FormEvent, useEffect, useState } from "react";
+import "react-dropzone/examples/theme.css";
+import { useApi, useConnection } from "arweave-wallet-kit";
+import Arweave from "arweave";
+
+const arweave = Arweave.init({});
 
 function App() {
+  const toast = useToast();
+  const [isLoading, setIsLoading] = useState(false);
   const { connected } = useConnection();
-  const api = useApi();
-  const [connectedAddress, setConnectedAddress] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const walletApi = useApi();
+  const [files, setFiles] = useState<(File & { preview: string })[]>([]);
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { "image/*": [] }, // Accept all image files
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
 
-  async function getConnectedAddress() {
+  const thumbs = files.map((file) => (
+    <Box
+      key={file.name}
+      display="inline-flex"
+      borderRadius={2}
+      border="1px solid #eaeaea"
+      marginBottom={8}
+      marginRight={8}
+      width={100}
+      height={100}
+      padding={4}
+      boxSizing="border-box"
+    >
+      <Flex minWidth={0} overflow="hidden">
+        <img
+          src={file.preview}
+          alt={file.name}
+          style={{
+            display: "block",
+            width: "auto",
+            height: "100%",
+          }}
+          // Revoke data URI after the image is loaded
+          onLoad={() => {
+            URL.revokeObjectURL(file.preview);
+          }}
+        />
+      </Flex>
+    </Box>
+  ));
+
+  function getErrorMessage(error: unknown): string {
+    if (
+      typeof error === "object" &&
+      error !== null &&
+      "message" in error &&
+      typeof (error as Record<string, unknown>).message === "string"
+    )
+      return (error as { message: string }).message;
+
     try {
-      // const address = await window.arweaveWallet.getActiveAddress();
-      const address = await api?.getActiveAddress();
-      if (address) setConnectedAddress(address);
-    } catch (error) {
-      console.error(error);
-      setErrorMessage("Failed to get active address: " + String(error));
+      return new Error(JSON.stringify(error)).message;
+    } catch {
+      return String(error);
     }
   }
 
-  useEffect(() => {
-    if (connected) {
-      void getConnectedAddress();
-    } else {
-      setErrorMessage("");
+  async function upload(e: FormEvent) {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const promises = files.map(async (file) => {
+        const data = await new Response(file).arrayBuffer();
+        const transaction = await arweave.createTransaction({
+          data,
+        });
+        transaction.addTag("App-Name", "Arweave Wallet Kit Demo");
+        transaction.addTag("Content-Type", file.type);
+        await walletApi?.sign(transaction);
+        await arweave.transactions.post(transaction);
+      });
+      await Promise.all(promises);
+      toast({
+        title: "Success",
+        description: "Uploaded sucessfully.",
+        status: "success",
+        duration: 9000,
+        position: "top-right",
+        isClosable: true,
+      });
+      setFiles([]);
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: getErrorMessage(error),
+        status: "error",
+        position: "top-right",
+        duration: 9000,
+        isClosable: true,
+      });
     }
+    setIsLoading(false);
+  }
 
+  useEffect(() => {
+    // Make sure to revoke the data URIs to avoid memory leaks, this will run on unmount
+    return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [connected]);
+  }, [files]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <ConnectButton profileModal={false} showBalance={true} />
-        </div>
-        <div>
-          Connected address with `useApi` hook:{" "}
-          <b style={{ color: "green" }}>{connectedAddress}</b>
-        </div>
-        {errorMessage && (
-          <div
-            style={{
-              color: "red",
-            }}
+    <Container py={8} maxW="6xl">
+      <form onSubmit={void upload}>
+        <VStack w="100%">
+          <Box className="container" w="full">
+            <Box
+              {...getRootProps({ className: "dropzone" })}
+              p={24}
+              w="full"
+              cursor="pointer"
+            >
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some images here, or click to select images</p>
+            </Box>
+            <Flex flexDirection="row" flexWrap="wrap" marginTop={16}>
+              {thumbs}
+            </Flex>
+          </Box>
+          <Button
+            colorScheme="blue"
+            isDisabled={files.length === 0 || !connected}
+            isLoading={isLoading}
+            loadingText="Uploading"
+            type="submit"
           >
-            Error: {errorMessage}
-          </div>
-        )}
-        <br />
-      </div>
-    </>
+            Upload
+          </Button>
+        </VStack>
+      </form>
+    </Container>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import { ArweaveWalletKit } from "arweave-wallet-kit";
 import App from "./App.tsx";
-import "./index.css";
+import NavBar from "./components/NavBar";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ArweaveWalletKit
-      config={{
-        permissions: ["ACCESS_ADDRESS"],
-        ensurePermissions: true,
-        appInfo: {
-          name: "Arweave-Wallet-Kit-Demo",
-        },
-      }}
-    >
-      <App />
-    </ArweaveWalletKit>
+    <ChakraProvider>
+      <ArweaveWalletKit
+        config={{
+          permissions: ["ACCESS_ADDRESS", "SIGN_TRANSACTION", "DISPATCH"],
+          ensurePermissions: true,
+          appInfo: {
+            name: "Arweave-Wallet-Kit-Demo",
+          },
+        }}
+      >
+        <NavBar />
+        <App />
+      </ArweaveWalletKit>
+    </ChakraProvider>
   </React.StrictMode>
 );
